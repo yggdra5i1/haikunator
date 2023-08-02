16f2f12eba34d040e0b216a0b60a9ecba45e905b
Maksim Shirokov <m5d3v@mail.ru>
2023-08-02 23:07:21 +0500

    Improves definitions, fix deprecated rand generation

--- main.go
+++ main.go
@@ -6,28 +6,30 @@ import (
 	"time"
 )
 
-var adjectives = []string{
-	"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer",
-	"icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient",
-	"twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing",
-	"broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering",
-	"bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small",
-	"sparkling", "thrumming", "shy", "wandering", "withered", "wild", "black",
-	"young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral",
-	"restless", "divine", "polished", "ancient", "purple", "lively", "nameless",
-}
+var (
+	adjectives = []string{
+		"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer",
+		"icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient",
+		"twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing",
+		"broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering",
+		"bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small",
+		"sparkling", "thrumming", "shy", "wandering", "withered", "wild", "black",
+		"young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral",
+		"restless", "divine", "polished", "ancient", "purple", "lively", "nameless",
+	}
 
-var nouns = []string{
-	"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-	"snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest",
-	"hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly",
-	"bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass",
-	"haze", "mountain", "night", "pond", "darkness", "snowflake", "silence",
-	"sound", "sky", "shape", "surf", "thunder", "violet",
-}
+	nouns = []string{
+		"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+		"snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest",
+		"hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly",
+		"bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass",
+		"haze", "mountain", "night", "pond", "darkness", "snowflake", "silence",
+		"sound", "sky", "shape", "surf", "thunder", "violet",
+	}
+)
 
 func Haikunate(tokenRange int, delimiter string) string {
-	rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 	sections := []string{
 		adjectives[rand.Intn(len(adjectives))],
 		nouns[rand.Intn(len(nouns))],
